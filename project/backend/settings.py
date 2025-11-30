cbb54284f8f335625d2a2dec8948710c2963e40e
RETR0-OS <RETR0-OS@users.noreply.github.com>
2025-11-30 03:57:37 -0700

    fix static assets

--- project/backend/settings.py
+++ project/backend/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files
